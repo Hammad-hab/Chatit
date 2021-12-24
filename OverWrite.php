19233ec10065526786f924b7ded03f11b8d02836
Hammad Ali Butt <88202647+Hammad-hab@users.noreply.github.com>
2021-12-24 13:33:48 +0500

    Update OverWrite.php

--- OverWrite.php
+++ OverWrite.php
@@ -2,7 +2,7 @@
 
 $ServerData1 = $_GET['TheValue'];
 
-$_WritingIntoAFile = file_put_contents('https://github.com/Hammad-hab/Json/DB.json', '{"data": {"Message1" : "'.$ServerData1.'"}}');
+$_WritingIntoAFile = file_put_contents('https://github.com/Hammad-hab/Json/blob/main/DB.json', '{"data": {"Message1" : "'.$ServerData1.'"}}');
 echo $_WritingIntoAFile;
 
 ?>
